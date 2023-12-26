4a86682511d926990ad2983e146293f24c502bbc
Tahir Civan <tahir.civan@samsonvt.com>
2023-12-26 13:50:26 +0100

    Update page files

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 module.exports = {
-	mode: 'development',
-	entry: [
-		'./index.ts',
-	],
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: [ 'ts-loader' ],
-				exclude: /node_modules/,
-			}
-		],
-	},
-	devServer: {
-		contentBase: path.resolve(__dirname, './assets'),
-	},
-	resolve: {
-		extensions: ['.ts', '.js'],
-	},
+  mode: 'development',
+  entry: './index.ts',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'), // Output directory
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './assets/index.html', // Path to your HTML file
+      filename: 'index.html', // Name of the output HTML file
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ['ts-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
 };
